80a75251470435e1ccf263974f62d5f5ee63a98b
micfun123 <54761507+micfun123@users.noreply.github.com>
2024-03-22 10:33:11 +0000

    Reciver clean up

--- main.py
+++ main.py
@@ -22,14 +22,15 @@ def send_message(sock, message):
 
 def receive_messages(sock):
     while True:
-        data = sock.recv(4096).decode()
-        data = data.strip("\n\r")
-        #clean the message
-        data = re.sub(r"\x02|\x1f|\x1d|\x1e", "", data)
-        #remove the hostname from the message
-        data = re.sub(r":.*?!", "", data)
-        print(data)
-
+        data = sock.recv(4096).decode().strip("\n\r")
+        # Clean the message
+        data = re.sub(r"[\x02\x1f\x1d\x1e]", "", data)
+        # Extract the sender's name and message content
+        match = re.match(r":(\S+)!\S+ PRIVMSG (\S+) :(.*)", data)
+        if match:
+            sender = match.group(1)
+            message = match.group(3)
+            print(f"{sender}: {message}")
 
 
 def main():
@@ -44,7 +45,6 @@ def main():
 
     send_thread.start()
     receive_thread.start()
-
 def send_loop(sock):
     while True:
         message = input()
@@ -62,18 +62,73 @@ def send_loop(sock):
             os.system('cls' if os.name == 'nt' else 'clear')
         elif message == "/here":
             sock.send(f"NAMES {channel}\r\n".encode())
+        elif message.startswith("/msg"):
+            parts = message.split(" ", 2)
+            recipient = parts[1]
+            message_content = parts[2]
+            sock.send(f"PRIVMSG {recipient} :{message_content}\r\n".encode())
+        elif message.startswith("/nick"):
+            new_nickname = message.split(" ")[1]
+            sock.send(f"NICK {new_nickname}\r\n".encode())
+        elif message.startswith("/quit"):
+            quit_message = " ".join(message.split(" ")[1:])
+            sock.send(f"QUIT :{quit_message}\r\n".encode())
+            break
+        elif message.startswith("/whois"):
+            target_user = message.split(" ")[1]
+            sock.send(f"WHOIS {target_user}\r\n".encode())
+        elif message.startswith("/away"):
+            away_message = " ".join(message.split(" ")[1:])
+            sock.send(f"AWAY :{away_message}\r\n".encode())
+        elif message.startswith("/topic"):
+            parts = message.split(" ", 2)
+            channel = parts[1]
+            new_topic = parts[2] if len(parts) > 2 else None
+            if new_topic:
+                sock.send(f"TOPIC {channel} :{new_topic}\r\n".encode())
+            else:
+                sock.send(f"TOPIC {channel}\r\n".encode())
+        elif message.startswith("/kick"):
+            parts = message.split(" ", 3)
+            channel = parts[1]
+            user = parts[2]
+            reason = parts[3] if len(parts) > 3 else None
+            if reason:
+                sock.send(f"KICK {channel} {user} :{reason}\r\n".encode())
+            else:
+                sock.send(f"KICK {channel} {user}\r\n".encode())
+        elif message.startswith("/invite"):
+            parts = message.split(" ", 2)
+            user = parts[1]
+            channel = parts[2]
+            sock.send(f"INVITE {user} {channel}\r\n".encode())
+        elif message.startswith("/mode"):
+            parts = message.split(" ", 2)
+            channel = parts[1]
+            mode = parts[2]
+            sock.send(f"MODE {channel} {mode}\r\n".encode())
+        elif message.startswith("/me"):
+            action = message.split(" ", 1)[1]
+            sock.send(f"PRIVMSG {channel} :\x01ACTION {action}\x01\r\n".encode())
         elif message == "/help":
-            print("""
-            /exit - exit the chat
-            /list - list all channels
-            /join <channel> - join a channel
-            /cls - clear the screen
-            /here - list all users in the channel
-            /help - list all commands
-            """)
+            print("Available commands:")
+            print("/list - List all channels")
+            print("/join <channel> - Join a channel")
+            print("/cls - Clear the screen")
+            print("/here - List users in the current channel")
+            print("/msg <user> <message> - Send a private message")
+            print("/nick <nickname> - Change your nickname")
+            print("/quit [<reason>] - Quit from the server")
+            print("/whois <user> - Get information about a user")
+            print("/away [<message>] - Set an away message")
+            print("/topic [<channel>] [<new_topic>] - Set or display the topic of a channel")
+            print("/kick <channel> <user> [<reason>] - Kick a user from a channel")
+            print("/invite <user> <channel> - Invite a user to a channel")
+            print("/mode <channel> <mode> - Set or display channel modes")
+            print("/me <action> - Perform an action in the channel")
         else:
             send_message(sock, message)
-            
+
     sock.close()
 
 
